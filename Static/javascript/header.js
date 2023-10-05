184432bdebec6d385725d7347ac68425dc28e3a2
AdrianoLMRS <adrianoLMRS@gmail.com>
2023-10-05 12:44:17 -0300

    1. Better nav-bar with CSS. 2. Padding and Size elements fixed. 3. Added SVG icons to the Media folder.

---
+++ Static/javascript/header.js
@@ -0,0 +1,10 @@
+// window.addEventListener("scroll", function() {
+//   const navBar = document.getElementById("nav-bar");
+//   if (window.scrollY > 50) {
+//     navBar.classList.add("fixed");
+//   } else {
+//     navBar.classList.remove("fixed");
+//   }
+// });
+
+console.log("Hello World!")
