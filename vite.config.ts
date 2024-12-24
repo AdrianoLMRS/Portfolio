50d9d76cfef1ed62823b6b36d4660ec4e5f2e7f6
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-12-24 12:24:37 -0300

    Folder structure & import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "src/assets/styles"),
+      "@img": path.resolve(__dirname, "src/assets/images"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utils": path.resolve(__dirname, "src/utils")
+    }
+  }
+});
