4c029e5bd5d69ac0fbeccf582e66e4c4647bc152
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-12-30 13:46:40 -0300

    Nav responsive

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from 'react';
 import { navItems } from './navItems';
-import './navbar.scss'
+import './navbar.scss';
 
-const positionNavbar = async () => {
+function getCssVariable(varName: string) : number {
+    return parseInt(getComputedStyle(document.documentElement).getPropertyValue(varName), 10);
+}
+
+const positionNavbar = () => {
     const nav = document.getElementById('navbar');
     const btn = document.getElementById('navBtn');
     if (nav && btn) {
@@ -12,27 +17,43 @@ const positionNavbar = async () => {
             nav.style.right = '-400px';
             btn.style.right = '0';
         }
-    } else console.log('Navbar not found...');
+    } else {
+        console.log('Navbar not found...');
+    }
 };
 
-
 function Navbar() {
+    const [isSidebar, setIsSidebar] = useState(false);
+    const smBreakpoint = getCssVariable('--sm-breakpoint');
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSidebar(window.innerWidth <= smBreakpoint);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
-        
         <header>
-            <nav className='sidebar' id='navbar'>
-            {navItems(
-                { name: "Home", href: "/" },
-                { name: "About", href: "/about" },
-                { name: "Projects", href: "/projects" },
-                { name: "Terminal", href: "/terminal" }
+            <nav className={isSidebar ? 'sidebar' : 'navbar'} id='navbar'>
+                {navItems(
+                    { name: 'Home', href: '/' },
+                    { name: 'About', href: '/about' },
+                    { name: 'Projects', href: '/projects' },
+                    { name: 'Terminal', href: '/terminal' }
                 )}
             </nav>
-            <button onClick={positionNavbar} type="button" className="nav-btn" id='navBtn'></button>
-            <hr className='header-hr' />
+            <button onClick={positionNavbar} type="button" className="nav-btn" id="navBtn"></button>
+            <hr className="header-hr" />
         </header>
     );
 }
 
-export default Navbar
+export default Navbar;
