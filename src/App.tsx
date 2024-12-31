c3146abab4f1d07490a0c67eefaa86cccbe283d3
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-12-31 14:26:47 -0300

    Footer.tsx init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import '@styles/App.scss'
 // import Header from '@components/header/Header';
 import Navbar from '@components/navbar/Navbar';
+import Footer from '@components/footer/Footer';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+function Footer() {
+    return(
+        <footer>
+            <h6>Hello World!</h6>
+        </footer>
+    );
+}
+
+export default Footer;
