f2e89b2ae9d34ae0b632703af668def2faa5ba7f
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-01-07 00:30:47 -0300

    Update about section styles
    Responsiveness + button "more"
    TODO: button make appear a popup with the full stack

--- src/components/main/sections/about/About.tsx
+++ src/components/main/sections/about/About.tsx
@@ -4,6 +4,8 @@ import Item from './itemImg'
 import './index.scss';
 
 function About() {
+    const itemImgSize : string = '96'; // In pixels 96px
+
     return (
         <section id='about' className="about">
             <div className="content">
@@ -15,30 +17,33 @@ function About() {
                     <h2>Stack atual</h2>
                     <div className="img-container">
                         <Item 
-                            src="https://img.icons8.com/office/96/react.png" 
+                            src={`https://img.icons8.com/office/${itemImgSize}/react.png`} 
                             alt="ReactJS Icon"
                             title="ReactJS"
                         />
                         <Item 
-                            src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-typescript-an-open-source-programming-language-developed-and-maintained-by-microsoft-logo-color-tal-revivo.png" 
+                            src={`https://img.icons8.com/external-tal-revivo-color-tal-revivo/${itemImgSize}/external-typescript-an-open-source-programming-language-developed-and-maintained-by-microsoft-logo-color-tal-revivo.png`} 
                             alt="Typescript Icon" 
                             title="Typescript" 
                         />
                         <Item 
-                            src="https://img.icons8.com/color/96/nodejs.png" 
+                            src={`https://img.icons8.com/color/${itemImgSize}/nodejs.png`} 
                             alt="NodeJS Icon" 
                             title="NodeJS" 
                         />
                         <Item 
-                            src="https://img.icons8.com/color/96/mongo-db.png" 
+                            src={`https://img.icons8.com/color/${itemImgSize}/mongo-db.png`} 
                             alt="MongoDB Icon" 
                             title="MongoDB" 
                         />
                         <Item 
-                            src="https://img.icons8.com/color/96/sass.png" 
+                            src={`https://img.icons8.com/color/${itemImgSize}/sass.png`} 
                             alt="Sass Icon" 
                             title="Sass" 
                         />
+                        <div className="item">
+                            <button onClick={() => {alert('WIP - Em desenvolvimento')}} className='button1' type="button">More</button>
+                        </div>
                     </div>
                 </div>
             </div>
