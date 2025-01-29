96bbbadd7694d03d015ff62cbaba2bfc1f1e7f78
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-01-29 17:28:43 -0300

    New tech cards + imgs

--- src/components/main/sections/tech/Tech.tsx
+++ src/components/main/sections/tech/Tech.tsx
@@ -12,21 +12,30 @@ function Tech() {
                 <h2>Tech Stack</h2>
                 <div className="stack">
                     {/* Frontend */}
+                    <Card imgSrc={src.html} techName="HTML" />
+                    <Card imgSrc={src.css} techName="CSS" />
+                    <Card imgSrc={src.markdown} techName="Markdown" />
                     <Card imgSrc={src.react} techName="React" />
                     <Card imgSrc={src.typescript} techName="TypeScript" />
                     <Card imgSrc={src.sass} techName="SASS" />
-                    <Card imgSrc={src.nodejs} techName="Node.js" />
+                    <Card imgSrc={src.python} techName="Python" />
+                    <Card imgSrc={src.c} techName="C" />
+
+                    {/* Databases */}
                     <Card imgSrc={src.mongodb} techName="MongoDB" />
                     <Card imgSrc={src.mysql} techName="MySQL" />
+                    <Card imgSrc={src.sqlite} techName="SQLite" />
+                    <Card imgSrc={src.redis} techName="Redis" />
                     
                     {/* Backend */}
                     <Card imgSrc={src.express} techName="Express.js" />
-                    <Card imgSrc={src.redis} techName="Redis" />
+                    <Card imgSrc={src.nodejs} techName="Node.js" />
                     
                     {/* Deployment & Tools */}
                     <Card imgSrc={src.docker} techName="Docker" />
                     <Card imgSrc={src.vite} techName="Vite" />
                     <Card imgSrc={src.aws} techName="AWS" />
+                    <Card imgSrc={src.jest} techName="Jest" />
                     <Card imgSrc={src.cmake} techName="CMake" />
                     <Card imgSrc={src.github} techName="GitHub Pages" />
                     <Card imgSrc={src.render} techName="Render" />
@@ -35,6 +44,7 @@ function Tech() {
                     <Card imgSrc={src.vscode} techName="VS Code" />
                     <Card imgSrc={src.postman} techName="Postman" />
                     <Card imgSrc={src.git} techName="Git" />
+                    <Card imgSrc={src.eslint} techName="Eslint" />
                     {/* <Card imgSrc={src.mongodbCompass} techName="MongoDB Compass" /> */}
                     
                     {/* OS */}
--- src/data/imgSrc.ts
+++ src/data/imgSrc.ts
@@ -5,6 +5,14 @@ const linksImgSize : string = String( 48 ); // In pixels 48px
 
 const imgSrc = {
     tech: {
+        c: `https://img.icons8.com/fluency/${itemImgSize}/c-programming.png`,
+        python: `https://img.icons8.com/color/${itemImgSize}/python--v1.png`,
+        jest: `https://img.icons8.com/external-tal-revivo-filled-tal-revivo/${itemImgSize}/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-filled-tal-revivo.png`,
+        sqlite: `https://img.icons8.com/ios-filled/${itemImgSize}/sqlite.png`,
+        eslint: `https://img.icons8.com/color/${itemImgSize}/eslint.png`,
+        markdown: `https://img.icons8.com/ios-filled/${itemImgSize}/markdown.png`,
+        css: `https://img.icons8.com/fluency/${itemImgSize}/css3.png`,
+        html: `https://img.icons8.com/color/${itemImgSize}/html-5--v1.png`,
         react: `https://img.icons8.com/office/${itemImgSize}/react.png`,
         typescript: `https://img.icons8.com/external-tal-revivo-color-tal-revivo/${itemImgSize}/external-typescript-an-open-source-programming-language-developed-and-maintained-by-microsoft-logo-color-tal-revivo.png`,
         nodejs: `https://img.icons8.com/color/${itemImgSize}/nodejs.png`,
@@ -18,7 +26,7 @@ const imgSrc = {
         aws: `https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/${itemImgSize}/external-amazon-web-services-a-subsidiary-of-amazon-that-provides-on-demand-cloud-computing-logo-shadow-tal-revivo.png`,
         cmake: `https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/${itemImgSize}/external-cmake-a-cross-platform-free-and-open-source-software-tool-logo-shadow-tal-revivo.png`,
         github: `https://img.icons8.com/color/${itemImgSize}/github.png`,
-        render: 'https://iili.io/2L33MH7.webp',
+        render: 'https://iili.io/2L33MH7.webp', // Fixed scale ...
         vscode: `https://img.icons8.com/color/${itemImgSize}/visual-studio-code-2019.png`,
         postman: `https://img.icons8.com/external-tal-revivo-color-tal-revivo/${itemImgSize}/external-postman-is-the-only-complete-api-development-environment-logo-color-tal-revivo.png`,
         git: `https://img.icons8.com/color/${itemImgSize}/git.png`,
